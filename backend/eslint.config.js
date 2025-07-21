aeea3d9ba5b7930072619912c01c5bde7798953b
ztevens <stevens@adedipe.dev>
2025-07-21 13:16:35 +0100

    fix: Actually delete problematic eslint.config.js file
    - Remove the eslint.config.js that was ignoring all files
    - ESLint will now use the working .eslintrc.js configuration
    - This should resolve the persistent 'all files are ignored' error

--- backend/eslint.config.js
+++
@@ -1,9 +0,0 @@
-export default [
-  {
-    files: ['**/*.{js,ts}'],
-    rules: {
-      // Minimal rules to avoid parsing issues
-    },
-    ignores: ['**/*'], // Ignore all files for now
-  }
-];
